d841fbea70abfde21494937b4c0088b2239434cb
Janice Leung <janice.leung2008@gmail.com>
2020-07-16 20:47:25 +1000

    Updates

---
+++ precache-manifest.f8a15aa513991e0a296e894ed8aeae53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352f2fd1b7e47107436ec569b6c26a50",
+    "url": "/IAMJANICE/index.html"
+  },
+  {
+    "revision": "4430dae703a243686da7",
+    "url": "/IAMJANICE/static/css/main.39dedbce.chunk.css"
+  },
+  {
+    "revision": "987c967bf53b365ea314",
+    "url": "/IAMJANICE/static/js/2.573f8f3e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/IAMJANICE/static/js/2.573f8f3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4430dae703a243686da7",
+    "url": "/IAMJANICE/static/js/main.47b9f438.chunk.js"
+  },
+  {
+    "revision": "12cc5e17d035a6a4e611",
+    "url": "/IAMJANICE/static/js/runtime-main.6636b42f.js"
+  }
+]);

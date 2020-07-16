928e4a44ac1ccd3e303e17b688adb6e88d86fcca
Janice Leung <janice.leung2008@gmail.com>
2020-07-16 19:23:53 +1000

    add profile image

--- src/components/HeroImage/HeroImage.js
+++ src/components/HeroImage/HeroImage.js
@@ -1,13 +1,11 @@
 import React from "react";
-import { Col, Container, Image } from "react-bootstrap";
+import { Image } from "react-bootstrap";
 
 function HeroImage() {
   return (
-      <Container style={{ minHeight: "80%" }}>
-        <Col sm={8}>
-              <Image src="https://github.com/Janice-cloud/IAMJANICE/blob/master/Images/IMG_5796.JPG"></Image>
-        </Col>
-      </Container>
+    
+    <Image className="image" alt="Hero Image"></Image>
+  
   );
 }
 
